c5a3e65b54c1c7c9aede21fc8752b72a83e7a2c4
Sam Baker <Sam.Baker101@gmail.com>
2023-08-02 16:51:59 -0400

    Tweaking ALU

--- Design/ALU.v
+++ Design/ALU.v
@@ -18,40 +18,54 @@ module ALU(
 	output reg [`REG_WIDTH - 1 : 0] dout, //douter hold register
 	output reg [`REG_WIDTH - 1 : 0] status_out,
 	output reg wout,
-	output reg wout_status,
-	output overflow,
-	output reg carry_out,
-	output half_carry
+	output reg wout_status
 	);
 	
+	reg carry_out;
+	reg opp_reg;
+
+	assign carry_in = status_in[`CARRY];
+
+	
+
+
 	//In the 6502 most of this logic is implemented with NANDs and NORs but Im not pressed about it
 	always @(posedge phi1) begin
+		if (opp_reg !== func) begin
+			wout = 1'b0;
+			opp_reg = func;
+		end
+
 		if (!reset_n) begin
 			dout = 8'h00;
-		end else if (func == `SUM) begin
-			if (carry_in)						//I know it's wierd, don't ask
-				{carry_out, dout} = a + b + 1;	//This is lazy but it'll do for now
-			else
-				{carry_out, dout} = a + b;
-			wout = 1'b1;
-		end else if (func == `AND) begin
-			dout = a&b;	
-			wout = 1'b1;
-		end else if (func == `OR) begin
-			dout = a|b;		
-			wout = 1'b1;
-		end else if (func == `XOR) begin
-			dout = ~(a & b) & (a | b);	
-			wout = 1'b1;
-		end else if (func == `SR) begin
-			dout = a << b;		
-			wout = 1'b1;
-		end else begin
-			dout = 8'hZZ;
-			wout = 1'b0;
+		end else if (wout == 0) begin 
+			if (func == `SUM) begin
+				if (carry_in)						//I know it's wierd, don't ask
+					{carry_out, dout} = a + b + 1;	//This is lazy but it'll do for now
+				else
+					{carry_out, dout} = a + b;
+				wout = 1'b1;
+			end else if (func == `AND) begin
+				dout = a&b;	
+				wout = 1'b1;
+			end else if (func == `OR) begin
+				dout = a|b;		
+				wout = 1'b1;
+			end else if (func == `XOR) begin
+				dout = ~(a & b) & (a | b);	
+				wout = 1'b1;
+			end else if (func == `SR) begin
+				dout = a << b;		
+				wout = 1'b1;
+			end else begin
+				dout = 8'hZZ;
+				wout = 1'b0;
+			end
+
+			status_out[`CARRY] = carry_out;
 		end
-		
 	end
+
 endmodule
 
 `endif
